fbe835b6925aaac779aafa6f775b59cfa535815f
virendra <softech.vire@gmail.com>
2024-03-06 08:57:05 +0530

    adding chnges local

--- controller/fileshare.js
+++ controller/fileshare.js
@@ -1,54 +1,46 @@
 const fileModal = require("../modal/fileshere");
-const multer = require("multer")
+const multer = require("multer");
 const path = require("path");
 const uploadFilePath = path.join(__dirname, "..", "uploadedFiles");
 // const { v4: uuidv4 } = require("uuid");
 const shortid = require("shortid");
-  const short_id = shortid.generate();
+const short_id = shortid.generate();
 // ----------To upload the files 3rd party middleware -----------
-
 
 let storage = multer.diskStorage({
   destination: (req, file, cb) => cb(null, uploadFilePath),
   filename: (req, file, cb) => {
-   
     cb(null, short_id + path.extname(file.originalname));
   },
 });
-const uploadFile = multer({ storage:storage }).single("attachment");
-
-
+const uploadFile = multer({ storage: storage }).single("attachment");
 //---------------------POST -----------------------------
 const uploadOneFile = async (req, res) => {
-  
-  await uploadFile(req, res, async(err) => {
-    
-    if(err){
+  await uploadFile(req, res, async (err) => {
+    if (err) {
       res.json({
         sucess: false,
-        massage:"error to upload the File", err,
+        massage: "error to upload the File",
+        err,
       });
     } else {
       console.log("File Uploaded Sucesfullly !");
       //------------------- below this ;ine or save modela as to save data in DB -------------------
-      const newFile =  new fileModal({
+      const newFile = new fileModal({
         filename: req.file.filename,
         path: req.file.path,
         size: req.file.size,
-      
       });
       const newFileInsrted = await newFile.save();
-      
 
-       res.json({
-         sucess: true,
-         massage: "File Uploaded Sucesfullly !",
-         file_id: newFileInsrted._id, // uuid for save file data
-       });
+      res.json({
+        sucess: true,
+        massage: "File Uploaded Sucesfullly !",
+        file_id: newFileInsrted._id, // uuid for save file data
+      });
     }
-  }) 
-  
-  
+  });
+
   // const insertData = new JobModal(req.body);
   // insertData.save();
   // res.json({
@@ -57,30 +49,59 @@ const uploadOneFile = async (req, res) => {
   // });
 };
 
-const generateLink = (req, res) => {
-   res.json({
-     sucess: true,
-     massage: "Generate like for File API !",
-   });
- }
-const downloadFiles = (req, res) => {
-   res.json({
-     sucess: true,
-     massage: "Download File API !",
-   });
- };
-const sendFileLinksOnEmail = (req, res) => {
-   res.json({
-     sucess: true,
-     massage: "send File Link to Email API !",
-   });
+const generateLink = async (req, res) => {
+  try {
+    const dbData = await fileModal.findById(req.params.udid);
+    if (!dbData) {
+      // Invalid file id
+      return res.status(404).json({
+        success: false,
+        message: "File does not exist for the give ID",
+      });
+    }
+    res.json({
+      sucess: true,
+      massage: "Generated Link for File",
+      size: dbData.size / 1000 + " kb",
+      downloadLink: `${process.env.API_BASE_URL_LOCAL}/files/download/${req.params.udid}`,
+    });
+  } catch (err) {
+    res.json({ error: err.message });
+  }
 };
+const downloadFiles = async (req, res) => {
+  try {
+    const file = await fileModal.findById(req.params.udid);
+if (!file) {
+  // Invalid file id
+  return res.status(404).json({
+    success: false,
+    message: "File does not exist for the give ID",
+  });
+}
+    // console.log(file.path);
 
+    // res.json({
+    //   sucess: true,
+    //   massage: "Downloaded File.",
+    //   filename: file.filename,
+    // });
+    await res.download(file.path);
+    
+  } catch (err) {
+    res.json({ error: err.message });
+  }
+};
+const sendFileLinksOnEmail = (req, res) => {
+  res.json({
+    sucess: true,
+    massage: "send File Link to Email API !",
+  });
+};
 
 module.exports = {
   uploadOneFile,
   generateLink,
   downloadFiles,
   sendFileLinksOnEmail,
-
 };
--- index.js
+++ index.js
@@ -4,17 +4,27 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const app = express();
 dotenv.config();
-mongoose
-  // .connect("mongodb://127.0.0.1:27017/shareFile")
-  .connect(
-    `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.3qnwaw3.mongodb.net/`
-  )
+if (process.env.SERVER=="LOCAL") {
+  mongoose
+    .connect("mongodb://127.0.0.1:27017/shareFile")
   .then(() => {
-    console.log("Database connected Successully.");
+    console.log("LOCAL Database connected Successully.");
   })
   .catch((err) => {
     console.log("Database connected failed ", err);
   });
+} else {
+  mongoose.connect(
+    `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.3qnwaw3.mongodb.net/`
+  )
+   .then(() => {
+    console.log("REMOTE Database connected Successully.");
+  })
+  .catch((err) => {
+    console.log("Database connected failed ", err);
+  });
+}
+
 
 app.use(express.json());
 
